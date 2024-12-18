5861eaa46ef3286490ad7d2c90b0043a1157eafb
Andriomdac <andriomdac@gmail.com>
2024-12-17 21:19:27 -0300

    att

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import PaymentMethod, Sale, SaleItem
 
-# Register your models here.
+@admin.register(PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ["method",]
+    search_fields = ["method",]
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ["total", "created_at", "updated_at", "payment_method",]
+    search_fields = ["total",]
+    
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ["sale", "product", "quantity", "price",]
+    search_fields = ["product",]
--- sales/models.py
+++ sales/models.py
@@ -1,1 +1,47 @@
 from django.db import models
+from products.models import Product
+from django.core.validators import MinValueValidator
+
+
+class PaymentMethod(models.Model):
+    method = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.method
+
+
+class Sale(models.Model):
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    payment_method = models.ForeignKey(
+        to=PaymentMethod,
+        on_delete=models.PROTECT,
+        related_name='sales'
+        )
+
+    def __str__(self):
+        return f"Venda {self.id} - Total: R${self.total:.2f} ({self.items.count()} itens) em {self.created_at}"
+
+
+class SaleItem(models.Model):
+    sale = models.ForeignKey(to=Sale, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='product_items'
+        )
+    quantity = models.IntegerField(default=1, validators=[MinValueValidator(1, message="A quantidade deve ser maior que zero")])
+    price = models.DecimalField(
+        blank=True,
+        null=True,
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01, message="O preço deve ser maior que zero")]
+        )
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} - {self.sale.id}"
+
